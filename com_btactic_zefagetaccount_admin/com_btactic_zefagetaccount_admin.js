668a31c9d67547a0b2feb235f49f55080b7ba43a
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2019-02-22 16:18:21 +0100

    ZaZefa global rename on com_btactic_zefagetaccount_admin.js

--- com_btactic_zefagetaccount_admin/com_btactic_zefagetaccount_admin.js
+++ com_btactic_zefagetaccount_admin/com_btactic_zefagetaccount_admin.js
@@ -16,54 +16,54 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 if(appNewUI && ZaSettings){
     if(window.console && window.console.log) console.log("Start loading com_btactic_zefagetaccount_admin.js");
-    function ZefaGetaccount() {
-        ZaItem.call(this,"ZefaGetaccount");
+    function ZaZefaGetaccount() {
+        ZaItem.call(this,"ZaZefaGetaccount");
         this._init();
-        this.type = "ZefaGetaccount";
+        this.type = "ZaZefaGetaccount";
     };
-    ZefaGetaccount.prototype = new ZaItem;
-    ZefaGetaccount.prototype.constructor = ZefaGetaccount;
+    ZaZefaGetaccount.prototype = new ZaItem;
+    ZaZefaGetaccount.prototype.constructor = ZaZefaGetaccount;
 
     ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW = ZaZimbraAdmin.VIEW_INDEX++;
 
-    ZaApp.prototype.getZefaGetaccountViewController =
+    ZaApp.prototype.getZaZefaGetaccountViewController =
         function() {
             if (this._controllers[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW] == null)
-                this._controllers[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW] = new ZefaGetaccountController(this._appCtxt, this._container);
+                this._controllers[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW] = new ZaZefaGetaccountController(this._appCtxt, this._container);
             return this._controllers[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW];
         }
 
-    ZefaGetaccount.TreeListener = function (ev) {
-        var zefaGetaccount = new ZefaGetaccount();
+    ZaZefaGetaccount.TreeListener = function (ev) {
+        var zefaGetaccount = new ZaZefaGetaccount();
 
         if(ZaApp.getInstance().getCurrentController()) {
-            ZaApp.getInstance().getCurrentController().switchToNextView(ZaApp.getInstance().getZefaGetaccountViewController(),ZefaGetaccountController.prototype.show, [zefaGetaccount]);
+            ZaApp.getInstance().getCurrentController().switchToNextView(ZaApp.getInstance().getZaZefaGetaccountViewController(),ZaZefaGetaccountController.prototype.show, [zefaGetaccount]);
         } else {
-            ZaApp.getInstance().getZefaGetaccountViewController().show(zefaGetaccount);
+            ZaApp.getInstance().getZaZefaGetaccountViewController().show(zefaGetaccount);
         }
     }
 
-    ZefaGetaccount.TreeModifier = function (tree) {
+    ZaZefaGetaccount.TreeModifier = function (tree) {
         var overviewPanelController = this ;
-        if (!overviewPanelController) throw new Exception("ZefaGetaccount.TreeModifier: Overview Panel Controller is not set.");
+        if (!overviewPanelController) throw new Exception("ZaZefaGetaccount.TreeModifier: Overview Panel Controller is not set.");
         if(ZaSettings.ENABLED_UI_COMPONENTS[ZaSettings.Client_UPLOAD_VIEW] || ZaSettings.ENABLED_UI_COMPONENTS[ZaSettings.CARTE_BLANCHE_UI]) {
             var parentPath = ZaTree.getPathByArray([ZaMsg.OVP_home, ZaMsg.OVP_toolMig]);
 
             var ti = new ZaTreeItemData({
                 parent:parentPath,
-                id:ZaId.getTreeItemId(ZaId.PANEL_APP,"magHV",null, "ZefaGetaccountHV"),
+                id:ZaId.getTreeItemId(ZaId.PANEL_APP,"magHV",null, "ZaZefaGetaccountHV"),
                 text: "ZEFA's Getaccount",
                 mappingId: ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW});
             tree.addTreeItemData(ti);
 
             if(ZaOverviewPanelController.overviewTreeListeners) {
-                ZaOverviewPanelController.overviewTreeListeners[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW] = ZefaGetaccount.TreeListener;
+                ZaOverviewPanelController.overviewTreeListeners[ZaZimbraAdmin._ZEFA_GETACCOUNT_VIEW] = ZaZefaGetaccount.TreeListener;
             }
         }
     }
 
     if(ZaOverviewPanelController.treeModifiers)
-        ZaOverviewPanelController.treeModifiers.push(ZefaGetaccount.TreeModifier);
+        ZaOverviewPanelController.treeModifiers.push(ZaZefaGetaccount.TreeModifier);
 
 }
 

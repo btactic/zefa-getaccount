b94accdcba350f3e0aced1711a56103ef579e1a1
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2019-02-22 17:21:08 +0100

    ZaZefaGetaccountTab acount TYPO fix

--- com_btactic_zefagetaccount_admin/ZaZefaGetaccountTab.js
+++ com_btactic_zefagetaccount_admin/ZaZefaGetaccountTab.js
@@ -14,30 +14,30 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-ZaZefaGetacountTab = function(parent, entry) {
+ZaZefaGetaccountTab = function(parent, entry) {
     if (arguments.length == 0) return;
-    ZaTabView.call(this, parent,"ZaZefaGetacountTab");
+    ZaTabView.call(this, parent,"ZaZefaGetaccountTab");
     ZaTabView.call(this, {
         parent:parent,
-        iKeyName:"ZaZefaGetacountTab",
+        iKeyName:"ZaZefaGetaccountTab",
         contextId:"ZEFA_GETACCOUNT"
     });
     this.setScrollStyle(Dwt.SCROLL);
 
-    var soapDoc = AjxSoapDoc.create("ZefaGetacount", "urn:ZefaGetacount", null);
+    var soapDoc = AjxSoapDoc.create("ZefaGetaccount", "urn:ZefaGetaccount", null);
     soapDoc.getMethod().setAttribute("action", "getAccounts");
     var csfeParams = new Object();
     csfeParams.soapDoc = soapDoc;
     csfeParams.asyncMode = true;
-    csfeParams.callback = new AjxCallback(ZaZefaGetacountTab.prototype.getAccountsCallback);
+    csfeParams.callback = new AjxCallback(ZaZefaGetaccountTab.prototype.getAccountsCallback);
     var reqMgrParams = {} ;
     resp = ZaRequestMgr.invoke(csfeParams, reqMgrParams);
 
     document.getElementById('ztab__ZEFA_GETACCOUNT').innerHTML = '<div style="padding-left:10px"><h1>ZEFAs Getaccount extension</h1>' +
-    '<h2>Create and remove shares</h2>This option allows you to share an entire account with another account. Useful for department and team mailboxes.<br><br><select id="ZefaGetacount-action" onchange="ZaZefaGetacountTab.prototype.uiUpdate(this.value);" ><option value="createShare">Share</option><option value="removeShare">Unshare</option></select> the account <input type="text" id="ZefaGetacount-account-a" list="ZefaGetacount-datalist" placeholder="user@domain.com">&nbsp;<span id="ZefaGetacount-withfrom">with</span>:&nbsp;<input type="text" id="ZefaGetacount-account-b" list="ZefaGetacount-datalist" placeholder="other-user@domain.com"><datalist id="ZefaGetacount-datalist"></datalist>&nbsp;&nbsp;<button id="ZefaGetacount-btnCreateShare">OK</button>' +
-    '<br><br><input type="checkbox" id="ZefaGetacount-disablePersonaCreation">Share only (skip configuring sendAs/persona\'s and mail filter).' +
+    '<h2>Create and remove shares</h2>This option allows you to share an entire account with another account. Useful for department and team mailboxes.<br><br><select id="ZefaGetaccount-action" onchange="ZaZefaGetaccountTab.prototype.uiUpdate(this.value);" ><option value="createShare">Share</option><option value="removeShare">Unshare</option></select> the account <input type="text" id="ZefaGetaccount-account-a" list="ZefaGetaccount-datalist" placeholder="user@domain.com">&nbsp;<span id="ZefaGetaccount-withfrom">with</span>:&nbsp;<input type="text" id="ZefaGetaccount-account-b" list="ZefaGetaccount-datalist" placeholder="other-user@domain.com"><datalist id="ZefaGetaccount-datalist"></datalist>&nbsp;&nbsp;<button id="ZefaGetaccount-btnCreateShare">OK</button>' +
+    '<br><br><input type="checkbox" id="ZefaGetaccount-disablePersonaCreation">Share only (skip configuring sendAs/persona\'s and mail filter).' +
     '<br><br><b>Permissions:</b>' +
-    '<br><select onchange="ZaZefaGetacountTab.prototype.uiUpdate(this.value);" id="ZefaGetacount-permissions">'+
+    '<br><select onchange="ZaZefaGetaccountTab.prototype.uiUpdate(this.value);" id="ZefaGetaccount-permissions">'+
     '<option value="r">r</option>' +
     '<option value="rw">rw</option>' +
     '<option value="rwix">rwix</option>' +
@@ -46,36 +46,36 @@ ZaZefaGetacountTab = function(parent, entry) {
     '<option value="none">none</option></select>' +    
     '<small style="font-size:11px;"><ul><li> (r)ead - search, view overviews and items</li><li> (w)rite - edit drafts/contacts/notes, set flags </li><li> (i)nsert - copy/add to directory, create subfolders action</li><li> (x) - workflow actions, like accepting appointments</li><li> (d)elete - delete items and subfolders, set \Deleted flag</li><li> (a)dminister - delegate admin and change permissions</li></ul></small>' +
     '<br><hr>' +
-    '<h2>Generate persona\'s</h2>This option allows you to generate a persona for each alias in the users account. <br><br><input type="text" id="ZefaGetacount-account-c" list="ZefaGetacount-datalist" placeholder="user@domain.com">&nbsp;&nbsp;<button id="ZefaGetacount-btnPersonaGen">OK</button>' +
+    '<h2>Generate persona\'s</h2>This option allows you to generate a persona for each alias in the users account. <br><br><input type="text" id="ZefaGetaccount-account-c" list="ZefaGetaccount-datalist" placeholder="user@domain.com">&nbsp;&nbsp;<button id="ZefaGetaccount-btnPersonaGen">OK</button>' +
     '<br><br><hr>' +
-    '<h2>Status</h2><div id="ZefaGetacount-status" style="color:#aaaaaa; font-style: italic;"></div></div>';   
+    '<h2>Status</h2><div id="ZefaGetaccount-status" style="color:#aaaaaa; font-style: italic;"></div></div>';   
     
-    ZaZefaGetacountTab.prototype.status('Loading auto completion...');
+    ZaZefaGetaccountTab.prototype.status('Loading auto completion...');
     
-    var btnCreateShare = document.getElementById('ZefaGetacount-btnCreateShare');
+    var btnCreateShare = document.getElementById('ZefaGetaccount-btnCreateShare');
     btnCreateShare.onclick = AjxCallback.simpleClosure(this.btnCreateRemoveShare);
     
-    var btnPersonaGen = document.getElementById('ZefaGetacount-btnPersonaGen');
+    var btnPersonaGen = document.getElementById('ZefaGetaccount-btnPersonaGen');
     btnPersonaGen.onclick = AjxCallback.simpleClosure(this.btnPersonaGen);
 }
 
 
-ZaZefaGetacountTab.prototype = new ZaTabView();
-ZaZefaGetacountTab.prototype.constructor = ZaZefaGetacountTab;
+ZaZefaGetaccountTab.prototype = new ZaTabView();
+ZaZefaGetaccountTab.prototype.constructor = ZaZefaGetaccountTab;
 
-ZaZefaGetacountTab.prototype.getTabIcon =
+ZaZefaGetaccountTab.prototype.getTabIcon =
     function () {
         return "ClientUpload" ;
     }
 
-ZaZefaGetacountTab.prototype.getTabTitle =
+ZaZefaGetaccountTab.prototype.getTabTitle =
     function () {
         return "Share Toolkit";
     }
 
-ZaZefaGetacountTab.prototype.getAccountsCallback = function (result) {
-   var dataList = document.getElementById('ZefaGetacount-datalist');
-   var users = result._data.Body.ZefaGetacountResponse.zefaGetaccountResult._content.split(";");
+ZaZefaGetaccountTab.prototype.getAccountsCallback = function (result) {
+   var dataList = document.getElementById('ZefaGetaccount-datalist');
+   var users = result._data.Body.ZefaGetaccountResponse.zefaGetaccountResult._content.split(";");
    
    users.sort();
    users.forEach(function(item) 
@@ -87,28 +87,28 @@ ZaZefaGetacountTab.prototype.getAccountsCallback = function (result) {
       // Add the <option> element to the <datalist>.
       dataList.appendChild(option);
    });
-   ZaZefaGetacountTab.prototype.status('Ready.');
+   ZaZefaGetaccountTab.prototype.status('Ready.');
    return;
 }
 
-ZaZefaGetacountTab.prototype.btnCreateRemoveShare = function () {
-    if(document.getElementById('ZefaGetacount-action').value == 'createShare')
+ZaZefaGetaccountTab.prototype.btnCreateRemoveShare = function () {
+    if(document.getElementById('ZefaGetaccount-action').value == 'createShare')
     {
-       ZaZefaGetacountTab.prototype.status('Creating share...');
+       ZaZefaGetaccountTab.prototype.status('Creating share...');
     }
     else
     {
-       ZaZefaGetacountTab.prototype.status('Removing share...');
+       ZaZefaGetaccountTab.prototype.status('Removing share...');
     }   
-    var accountA = document.getElementById('ZefaGetacount-account-a').value;
-    var accountB = document.getElementById('ZefaGetacount-account-b').value;
-    var skipPersonaCreation = document.getElementById('ZefaGetacount-disablePersonaCreation').checked;
-    var permissions = document.getElementById('ZefaGetacount-permissions').value;
+    var accountA = document.getElementById('ZefaGetaccount-account-a').value;
+    var accountB = document.getElementById('ZefaGetaccount-account-b').value;
+    var skipPersonaCreation = document.getElementById('ZefaGetaccount-disablePersonaCreation').checked;
+    var permissions = document.getElementById('ZefaGetaccount-permissions').value;
     
     if(accountA && accountB && (accountA !== accountB))
     {
-       var soapDoc = AjxSoapDoc.create("ZefaGetacount", "urn:ZefaGetacount", null);
-       soapDoc.getMethod().setAttribute("action", document.getElementById('ZefaGetacount-action').value);
+       var soapDoc = AjxSoapDoc.create("ZefaGetaccount", "urn:ZefaGetaccount", null);
+       soapDoc.getMethod().setAttribute("action", document.getElementById('ZefaGetaccount-action').value);
        soapDoc.getMethod().setAttribute("accounta", accountA);
        soapDoc.getMethod().setAttribute("accountb", accountB);
        soapDoc.getMethod().setAttribute("skipPersonaCreation", skipPersonaCreation);
@@ -116,66 +116,66 @@ ZaZefaGetacountTab.prototype.btnCreateRemoveShare = function () {
        var csfeParams = new Object();
        csfeParams.soapDoc = soapDoc;
        csfeParams.asyncMode = true;
-       csfeParams.callback = new AjxCallback(ZaZefaGetacountTab.prototype.zefaGetaccountDefaultCallback);
+       csfeParams.callback = new AjxCallback(ZaZefaGetaccountTab.prototype.zefaGetaccountDefaultCallback);
        var reqMgrParams = {} ;
        resp = ZaRequestMgr.invoke(csfeParams, reqMgrParams);
     }   
     else
     {
-       ZaZefaGetacountTab.prototype.status('Select or type 2 different email addresses.');
+       ZaZefaGetaccountTab.prototype.status('Select or type 2 different email addresses.');
     }
 }   
 
-ZaZefaGetacountTab.prototype.btnPersonaGen = function () {
-    ZaZefaGetacountTab.prototype.status('Creating persona\'s...');
+ZaZefaGetaccountTab.prototype.btnPersonaGen = function () {
+    ZaZefaGetaccountTab.prototype.status('Creating persona\'s...');
 
-    var accountA = document.getElementById('ZefaGetacount-account-c').value;
+    var accountA = document.getElementById('ZefaGetaccount-account-c').value;
   
     if(accountA)
     {
-       var soapDoc = AjxSoapDoc.create("ZefaGetacount", "urn:ZefaGetacount", null);
+       var soapDoc = AjxSoapDoc.create("ZefaGetaccount", "urn:ZefaGetaccount", null);
        soapDoc.getMethod().setAttribute("action", "createPersonas");
        soapDoc.getMethod().setAttribute("accounta", accountA);
        var csfeParams = new Object();
        csfeParams.soapDoc = soapDoc;
        csfeParams.asyncMode = true;
-       csfeParams.callback = new AjxCallback(ZaZefaGetacountTab.prototype.zefaGetaccountDefaultCallback);
+       csfeParams.callback = new AjxCallback(ZaZefaGetaccountTab.prototype.zefaGetaccountDefaultCallback);
        var reqMgrParams = {} ;
        resp = ZaRequestMgr.invoke(csfeParams, reqMgrParams);
     }   
     else
     {
-       ZaZefaGetacountTab.prototype.status('Select or type email address.');
+       ZaZefaGetaccountTab.prototype.status('Select or type email address.');
     }
 }   
   
    
-ZaZefaGetacountTab.prototype.zefaGetaccountDefaultCallback = function (result) {
-   ZaZefaGetacountTab.prototype.status('Ready. '+result._data.Body.ZefaGetacountResponse.zefaGetaccountResult._content);
+ZaZefaGetaccountTab.prototype.zefaGetaccountDefaultCallback = function (result) {
+   ZaZefaGetaccountTab.prototype.status('Ready. '+result._data.Body.ZefaGetaccountResponse.zefaGetaccountResult._content);
 }  
 
-ZaZefaGetacountTab.prototype.status = function (statusText) {
-   document.getElementById('ZefaGetacount-status').innerHTML = statusText;
+ZaZefaGetaccountTab.prototype.status = function (statusText) {
+   document.getElementById('ZefaGetaccount-status').innerHTML = statusText;
 }
 
-ZaZefaGetacountTab.prototype.uiUpdate = function (value) {
+ZaZefaGetaccountTab.prototype.uiUpdate = function (value) {
    if(value == 'createShare')
    {
-      document.getElementById('ZefaGetacount-withfrom').innerHTML = 'with';
-      document.getElementById('ZefaGetacount-permissions').value = 'rwixd';
-      document.getElementById('ZefaGetacount-permissions').disabled = false;
+      document.getElementById('ZefaGetaccount-withfrom').innerHTML = 'with';
+      document.getElementById('ZefaGetaccount-permissions').value = 'rwixd';
+      document.getElementById('ZefaGetaccount-permissions').disabled = false;
    }
    else if(value == 'none')
    {
-      document.getElementById('ZefaGetacount-withfrom').innerHTML = 'from';
-      document.getElementById('ZefaGetacount-permissions').value = 'none';
-      document.getElementById('ZefaGetacount-permissions').disabled = true;
-      document.getElementById('ZefaGetacount-action').value = 'removeShare';
+      document.getElementById('ZefaGetaccount-withfrom').innerHTML = 'from';
+      document.getElementById('ZefaGetaccount-permissions').value = 'none';
+      document.getElementById('ZefaGetaccount-permissions').disabled = true;
+      document.getElementById('ZefaGetaccount-action').value = 'removeShare';
    }
    else if(value == 'removeShare')
    {
-      document.getElementById('ZefaGetacount-withfrom').innerHTML = 'from';
-      document.getElementById('ZefaGetacount-permissions').value = 'none';
-      document.getElementById('ZefaGetacount-permissions').disabled = true;
+      document.getElementById('ZefaGetaccount-withfrom').innerHTML = 'from';
+      document.getElementById('ZefaGetaccount-permissions').value = 'none';
+      document.getElementById('ZefaGetaccount-permissions').disabled = true;
    }
 }
